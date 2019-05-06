de51888e2abde3bd90dd6ee91b8d993f9c3023a8
JesseCao86 <48713146+JesseCao86@users.noreply.github.com>
2019-05-06 17:52:23 +1000

    Update Assignment1Question1.py

--- Assignment1Question1.py
+++ Assignment1Question1.py
@@ -1,9 +1,12 @@
 # Read the file and save it in memory.
 # And for convenience, we use utf-8 to encode it
+# Run it first on AWS and then double line breaks.
 with open("twain.txt","r",encoding="utf-8") as f:
     lines = f.readlines()
 
+
 # Re-write the file and replace “Huck” into “HucK”
+# Run it later on AWS.
 with open("twain.txt","w",encoding="utf-8") as f_w:
     for line in lines:
         if "Huck" in line:
